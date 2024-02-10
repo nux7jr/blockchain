5dab7d3ed9c371f179a2b807518d659fe8340e85
nux7jr <nux7jr@gmail.com>
2024-02-10 12:06:19 +0700

    done

---
+++ index.js
@@ -0,0 +1,71 @@
+const crypto = require('crypto');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return crypto.createHash('sha256').update(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).digest('hex');
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/2022", "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let myBlockchain = new Blockchain();
+myBlockchain.addBlock(new Block(1, "02/01/2022", { amount: 4 }));
+myBlockchain.addBlock(new Block(2, "03/01/2022", { amount: 8 }));
+myBlockchain.addBlock(new Block(2, "04/01/2022", { amount: 9 }));
+console.log('Blockchain valid? ' + myBlockchain.isChainValid());
+
+myBlockchain.chain[1].data = { amount: 100 };
+
+console.log('Blockchain valid? ' + myBlockchain.isChainValid());
